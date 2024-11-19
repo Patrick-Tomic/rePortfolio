15b97964d19c2e8573a2906315f40b2f4229ebdd
Patrick-Tomic <pat12tomic@gmail.com>
2024-11-19 03:08:36 -0500

    startup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+backgroundColor:{
+  'header':'#13274F'
+},
+    extend: {
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
