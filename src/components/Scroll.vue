f777e7756f68c93ada5ae18120c5b2d28e0b9871
Patrick-Tomic <pat12tomic@gmail.com>
2024-12-07 06:39:57 -0500

    need to sleep a little

---
+++ src/components/Scroll.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  const arr = ['S','C','R','O','L','L']
+ 
+</script>
+<template>
+<span v-for="(letter, index) in arr"  style="animation-delay: (0.5+index/10)+{${`s`}};">{{ letter }}</span>
+</template>
+
+<style>
+</style>
